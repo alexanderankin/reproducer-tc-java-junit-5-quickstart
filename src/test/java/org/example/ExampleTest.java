737acdfeb0f63938bf658f73f38582f9276a9b95
David Ankin <daveankin@gmail.com>
2024-01-04 14:33:13 -0500

    follow gradle tutorial

---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+    @Test
+    void test() {
+        assertEquals(4, 2 + 2);
+    }
+}
