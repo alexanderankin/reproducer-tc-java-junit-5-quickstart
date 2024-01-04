31b3b4ed784581d919f7379990e2e309cee74790
David Ankin <daveankin@gmail.com>
2024-01-04 14:42:03 -0500

    test from step 5 runs

--- src/test/java/org/example/RedisBackedCacheIntTest.java
+++ src/test/java/org/example/RedisBackedCacheIntTest.java
@@ -2,18 +2,33 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // actually "RedisBackedCacheIntTestStep0"
 // docker run --rm -p 6379:6379 -it redis:alpine
+@Testcontainers
 public class RedisBackedCacheIntTest {
     private RedisBackedCache underTest;
 
+    // container {
+    @Container
+    public GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+        .withExposedPorts(6379);
+
+    // }
+
     @BeforeEach
     public void setUp() {
-        // Assume that we have Redis running locally?
-        underTest = new RedisBackedCache("localhost", 6379);
+        String address = redis.getHost();
+        Integer port = redis.getFirstMappedPort();
+
+        // Now we have an address and port for Redis, no matter where it is running
+        underTest = new RedisBackedCache(address, port);
     }
 
     @Test
